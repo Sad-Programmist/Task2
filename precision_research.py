d6686ca62f09ffb2e2b65a7d1c25370b478cbfe8
Яна <solodovnikovasyv@yandex.ru>
2022-03-11 17:36:56 +0300

    Add function parameter (precision_research.py).

--- precision_research.py
+++ precision_research.py
@@ -1,26 +1,27 @@
 from simple_methods import *
+from functions import *
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def error_function(function, a, b, n):
+def error_function(integral_function, function, a, b, n):
     result = []
-    true_value = function(a, b, 10000)
+    true_value = integral_function(function, a, b, 10000)
     for index in n:
-        result.append(np.abs(function(a, b, index) - true_value))
+        result.append(np.abs(integral_function(function, a, b, index) - true_value))
     return result
 
 
-def draw_function(a, b, n, step):
+def draw_function(function, a, b, n, step):
     list_n = np.arange(1, n, step)
     h = (b-a)/list_n
 
     fig, ax = plt.subplots()
-    ax.plot(h, error_function(boole_integral, a, b, list_n), c="deepskyblue", label="boole")
-    ax.plot(h, error_function(cubic_parabola_integral, a, b, list_n), c="lawngreen", label="cubic_parabola")
-    ax.plot(h, error_function(rectangle_integral, a, b, list_n), c="r", label="rectangle")
-    ax.plot(h, error_function(trapezoid_integral, a, b, list_n), c="darkorange", label="trapezoid")
-    ax.plot(h, error_function(parabola_integral, a, b, list_n), c="darkviolet", label="parabola")
+    ax.plot(h, error_function(boole_integral, function, a, b, list_n), c="deepskyblue", label="boole")
+    ax.plot(h, error_function(cubic_parabola_integral, function, a, b, list_n), c="lawngreen", label="cubic_parabola")
+    ax.plot(h, error_function(rectangle_integral, function, a, b, list_n), c="r", label="rectangle")
+    ax.plot(h, error_function(trapezoid_integral, function, a, b, list_n), c="darkorange", label="trapezoid")
+    ax.plot(h, error_function(parabola_integral, function, a, b, list_n), c="darkviolet", label="parabola")
 
     ax.set(xlabel='h', ylabel='error', title='Precision research')
     plt.xlim(0, 0.15)
@@ -36,4 +37,4 @@ if __name__ == '__main__':
     b = math.pi / 6
     n = 1000
     step = 5
-    draw_function(a, b, n, step)
+    draw_function(function1, a, b, n, step)
